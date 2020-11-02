14ab91292f017e3b1f0063e57e18a9e619d41d4a
coconutpotato123 <73363585+coconutpotato123@users.noreply.github.com>
2020-11-02 16:22:00 -0500

    create hero and tile map

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
+// Initialize 
+let heroImg = sprites.duck.duck2
+let enemyImg = sprites.builtin.clownFish2
 
+
+
+tiles.setTilemap(tilemap`level`)
+// Create hero sprite
+
+let hero = sprites.create(heroImg, SpriteKind.Player)
+
+tiles.placeOnRandomTile(hero, sprites.dungeon.collectibleInsignia)
+controller.moveSprite(hero)
+scene.cameraFollowSprite(hero)
---
+++ tilemap.g.ts
@@ -0,0 +1,47 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers.registerTilemapFactory(function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level": return tiles.createTilemap(hex`100020000003070707070707070707070707010000050e0e0e0e0e0e0e0e0e0e0d0e060000050e0e0e0e0e0e0e0e0e0e0e0e060000050e0e0e0e0309090909090909060000050e0e0e0e0a0e0e0e0e0e0e0e060000050e0e0e0e0a0e0e0e0e0e0e0e060000050e0e0e0e0a0e0e0e03080808060000050e0e0e0e0a0e0e0e060e0e0e060000050e0e0e0e0a0e0e0e060e0e0e060000050e0e0e0e0a0e0e0e060e0e0e060000050e0e0e0e0a0e0e0e060e0e0e060000050e0e0e0e0a0e0e0e060e0e0e060000050e0e0e08020e0e0e0e0e0e0e060000050e0e0e0e0e0e0e0e0e0e0e0e060000050e0e0e0e0e0e0e0e0e0e0e0e060000050e0e0e0e0e0e0e0e0e0e0e0e060000050808080e0e0e030808080808060000050e0e0e0e0e0e0b0e0e0e0e0e060000050e0e0e0e0e0e0b0e0e0e0e0e060000050e0e0e0e0e0e0b0e0e0e0e0e060000050e0e0e0e0e0e0b0e0e0e0e0e060000050e0e0e0808080b0e0e0e0e0e060000050e0e0e0e0e0e0e0e0e0e0e0e060000050e0e0e0e0e0e0e0e0e0e0e0e060000050e0e0e0e0e0e0e0e0e0e0e0e060000050e0e0e080808080808080808060000050e0e0e0e0e0e0e0e0e0e0e0e0600000a0e0e0e0e0e0e0e0e0e0e0e0e06000005080808080e0e0e0e0e0e0c0e060000050e0e0e0e0e0e0e0e0e0e0e0e06000004080808080808080808080808020000000000000000000000000000000000`, img`
+.22222222222222.
+.2............2.
+.2............2.
+.2....222222222.
+.2....2.......2.
+.2....2.......2.
+.2....2...22222.
+.2....2...2...2.
+.2....2...2...2.
+.2....2...2...2.
+.2....2...2...2.
+.2....2...2...2.
+.2...22.......2.
+.2............2.
+.2............2.
+.2............2.
+.2222...2222222.
+.2......2.....2.
+.2......2.....2.
+.2......2.....2.
+.2......2.....2.
+.2...2222.....2.
+.2............2.
+.2............2.
+.2............2.
+.2...2222222222.
+.2............2.
+.2............2.
+.22222........2.
+.2............2.
+.22222222222222.
+................
+`, [myTiles.transparency16,sprites.dungeon.purpleOuterNorthEast,sprites.dungeon.purpleOuterSouthWest,sprites.dungeon.purpleOuterNorthWest,sprites.dungeon.purpleOuterSouthEast,sprites.dungeon.purpleOuterWest0,sprites.dungeon.purpleOuterEast0,sprites.dungeon.purpleOuterNorth1,sprites.dungeon.purpleOuterSouth1,sprites.dungeon.purpleOuterSouth0,sprites.dungeon.purpleOuterWest1,sprites.dungeon.purpleOuterEast1,sprites.dungeon.collectibleBlueCrystal,sprites.dungeon.collectibleInsignia,sprites.dungeon.floorLight0], TileScale.Sixteen)
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
